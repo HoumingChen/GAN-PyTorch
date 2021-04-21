b41307ac08063c2910efe14d6cee93878f6d24fc
HoumingChen <54627930+HoumingChen@users.noreply.github.com>
2021-04-21 17:35:17 -0400

    Create createGaussianNumbers.py

---
+++ createGaussianNumbers.py
@@ -0,0 +1,5 @@
+# This file will implement functions for Gaussian distributions.
+import numpy as np
+
+def g(size, loc = 0.0, scale = 1.0):
+    return np.random.normal(size=size, loc = 0.0, scale = 1.0)
